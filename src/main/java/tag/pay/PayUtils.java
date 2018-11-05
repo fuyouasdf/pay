fe6164faafca4d10639b1dd04bc3bbd09dd9e1e6
fy_ <249010929@qq.com>
2018-11-05 16:09:10 +0800

    first commit

---
+++ src/main/java/tag/pay/PayUtils.java
@@ -0,0 +1,36 @@
+package tag.pay;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+/**
+ * Created by fuyou on 2017/2/15.
+ */
+public class PayUtils {
+    /**
+     * 获取客户端ip
+     * @return
+     *      成功 ip
+     *      失败 null
+     */
+    public static String getIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        // if (!inetAddress.isLoopbackAddress() && inetAddress
+                        // instanceof Inet6Address) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
